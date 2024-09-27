b38c5032890b0fa08a28574b79cf792e9be509f0
Siddharth <113140500+siddharthverse@users.noreply.github.com>
2024-09-26 20:41:59 -0400

    Playable Tic Tac Toe game

--- src/components/TicTacToe/Board.tsx
+++ src/components/TicTacToe/Board.tsx
@@ -7,6 +7,9 @@ type SquareProps = {
 };
 
 const Square = ({ value, onSquareClick }: SquareProps) => {
+  // {
+  //   console.log(`square fn ${value}`);
+  // }
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
@@ -14,20 +17,40 @@ const Square = ({ value, onSquareClick }: SquareProps) => {
   );
 };
 
-// https://react.dev/learn/tutorial-tic-tac-toe
-// Completing the game
-
 const Board = () => {
   const [square, setSquare] = useState(Array(9).fill(null));
+  const [xIsNext, setXIsNext] = useState(true);
 
+  // handle board click
   function handleClick(i: number) {
+    console.log(`board handle click SQUARE ${i}`);
+    if (square[i] || calculateWinner(i)) {
+      console.log('Same click or maybe game over');
+      return;
+    }
+
+    // handle X or O
     const nextSquares = square.slice();
-    nextSquares[i] = 'X';
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    } else {
+      nextSquares[i] = 'O';
+    }
+    setXIsNext(!xIsNext);
     setSquare(nextSquares);
+  }
+
+  const winner = calculateWinner(square);
+  let status;
+  if (winner) {
+    status = `Winner is: ${winner}`;
+  } else {
+    status = `Next player: ${xIsNext ? 'X' : 'O'}`;
   }
 
   return (
     <>
+      <div className="status">{status}</div>
       <div className="board-row">
         <Square value={square[0]} onSquareClick={() => handleClick(0)} />
         <Square value={square[1]} onSquareClick={() => handleClick(1)} />
@@ -45,6 +68,26 @@ const Board = () => {
       </div>
     </>
   );
+};
+
+const calculateWinner = (squares: any) => {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 };
 
 export default Board;
